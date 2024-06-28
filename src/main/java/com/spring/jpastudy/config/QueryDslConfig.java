e060320c07bec5c129e620b8ed1ea021a4e4c7e4
kimbyeongmo <kbmomo9999@naver.com>
2024-06-28 09:50:13 +0900

    # 19. 커스텀 레포지토리

--- src/main/java/com/spring/jpastudy/config/QueryDslConfig.java
+++ src/main/java/com/spring/jpastudy/config/QueryDslConfig.java
@@ -14,7 +14,7 @@ public class QueryDslConfig {
     @PersistenceContext
     private EntityManager em;
 
-    @Bean // 외부라이브러리를 스프링 컨ㅌ이너에 관리시키는 설정
+    @Bean // 외부라이브러리를 스프링 컨테이너에 관리시키는 설정
     public JPAQueryFactory jpaQueryFactory(){
         return new JPAQueryFactory(em);
     }
